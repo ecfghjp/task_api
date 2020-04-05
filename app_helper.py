9eb235be83e0f2ff3f093ed3ce0dc6f0d30a5e04
Abhishek Sharma <abhishek.sharma@dwp.gov.uk>
2020-04-05 12:03:23 +0100

    test load file added

--- app_helper.py
+++ app_helper.py
@@ -7,3 +7,9 @@ class app_helper:
 
     def convert_to_json(data):
         return dumps(data)
+    
+    #load test data during testing phase
+    def test_load_json():
+        with open('GITHub/task_api/testload.json') as json_file:
+            test_data = json.load(json_file)
+        return test_data
